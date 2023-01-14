b1e6fa86c9602245529ab9b44fcb4be4509ae185
Muhamaadjon <mmuhammadi754@gmail.com>
2023-01-14 13:05:48 +0500

    add main.js

---
+++ main.js
@@ -0,0 +1,75 @@
+// task 3
+// let text= "aaa@bbb@ccc"
+// let result=text.replaceAll("@","!")
+// console.log(result)
+// alert(result)
+
+// task 4
+// let text= "aaa bbb ccc"
+// let result=text.substr(3, 5)
+// let result1=text.substring(3, 7)
+// let result2=text.slice(3, 7)
+// console.log(result)
+// alert(result)
+// console.log(result1)
+// alert(result1)
+// console.log(result2)
+// alert(result2)
+
+
+// task 5
+// let text= "js"
+// let result=text.toUpperCase()
+// console.log(result)
+// alert(result)
+
+
+// task 6
+// let text= "JS"
+// let result=text.toLowerCase()
+// console.log(result)
+// alert(result)
+
+// task 7
+// let text= "я учу javascript!"
+// let result=text.lastIndexOf("!")
+// console.log(result)
+// alert(result)
+
+// // task 8
+// let text= "я учу javascript!"
+// let result=text.substr(2, 4)
+// let result1=text.substring(2, 6)
+// let result2=text.slice(2, 6)
+
+// let result3=text.substr(5, 10)
+// let result4=text.substring(6, 16)
+// let result5=text.slice(6, 16)
+
+// console.log(result)
+// alert(result)
+// console.log(result3)
+// alert(result3)
+// console.log(result1)
+// alert(result1)
+
+// console.log(result4)
+// alert(result4)
+// console.log(result2)
+// alert(result2)
+// console.log(result5)
+// alert(result5)
+
+
+// task 9
+// let text= "я учу javascript!"
+// let result=text.search("учу")
+// console.log(result)
+// alert(result)
+
+
+// task 10
+// let text= "я учу javascript!"
+// let result=text.split(" ")
+// console.log(result)
+// alert(result)
